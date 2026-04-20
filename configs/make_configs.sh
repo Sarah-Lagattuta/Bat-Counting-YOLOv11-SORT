3d2fad3a59c0e056ba43d795d55e1a5b955fa561
Sarah Lagattuta <sklaga24@gmail.com>
2026-04-20 16:19:41 -0700

    Update make_configs.sh

--- configs/make_configs.sh
+++ configs/make_configs.sh
@@ -36,14 +36,14 @@ while IFS='|' read -r site file coords; do
 
       {
         echo "tracking:"
-        echo "  detection_confidence: 0.18"
+        echo "  detection_confidence: 0.15"
         echo "  model_file: \"$model_file\""
         echo "  results_path: \"$csv\""
         echo "  imgsz: 1280"
         echo "  sort:"
         echo "    iou_threshold: 0.20"
         echo "    max_age: 30"
-        echo "    min_hits: 3"
+        echo "    min_hits: 4"
         echo "  video_files:"
         echo "  - amplification: 1.0"
         echo "    path: \"$PRJ/videos/$file\""
